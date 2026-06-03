91e9a60bdc7921dc0edc6e855eeb2c7e1be98350
Atul Rao <02atulrao@gmail.com>
2026-06-04 00:41:32 +0530

    Add Self-ping code to wake-up render server in ervery 10 minutes

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
+const axios = require("axios");
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
 
@@ -10,3 +11,9 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+setInterval(() => {
+  axios.get("https://productr-assignment-for-internship.onrender.com")
+    .then(() => console.log("🔄 Self-ping successful"))
+    .catch(err => console.log("❌ Self-ping failed:", err.message));
+}, 10 * 60 * 1000);
